96c3ea7e05fa40c031876fe4f228a67d5c335a17
pavel <paveljinarpopei@gamil.com>
2023-07-05 21:48:49 +0300

    commit

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.post('/login', async (req, res) => {
   try {
     username = req.body.username;
     const psw = req.body.password;
-    const users = await client.query('SELECT usname, parola FROM users WHERE usname = $1', [username]);
-    const validpsw = users.rows[0].parola;
-    if ((users.rows.length > 0 && username === users.rows[0].usname) && (psw === validpsw)) {
+    const users = await client.query('SELECT usname FROM users WHERE usname = $1', [username]);
+    const validpsw = await client.query('SELECT password FROM users WHERE usname = $1', [username]);
+    if ((users.rows.length > 0 && username === users.rows[0].usname) && (psw === validpsw.rows[0].password)) {
       res.render('startpage')
     } else {
-      res.send("Username sau parolă greșite");
+      res.send("Wrong username or password");
     }
   } catch (error) {
     console.log(error);
@@ -39,13 +39,13 @@ app.post('/register', async (req, res) => {
     const document = await client.query('SELECT usname FROM users WHERE usname = $1', [username]);
     if (document.rowCount === 0) {
       if (passw === repetpsw) {
-        client.query('INSERT INTO users (usname, parola) VALUES ($1, $2)', [username, passw]);
+        client.query('INSERT INTO users (usname, password) VALUES ($1, $2)', [username, passw]);
         res.render('startpage')
       } else {
-        res.send("Parolă greșita");
+        res.send("Wrong passwod");
       }
     } else {
-      res.send("Username-ul exista deja");
+      res.send("Username already exist");
     }
 
   } catch (error) {
@@ -55,30 +55,47 @@ app.post('/register', async (req, res) => {
 
 app.get('/profile/viewProfile', async (req, res) => {
   try {
-    const response = await client.query(`SELECT descriere FROM users WHERE usname = $1`, [username])
-    const description = response.rows[0] || {descriere:"nu exista o descriere"};
-    res.render('profile', {description});
+    const response = await client.query(`SELECT description FROM users WHERE usname = $1`, [username])
+    const description = response.rows[0] || { description: "Description do not exist" };
+    res.render('profile', { description });
   } catch (error) {
     res.status(404).send("Some server errors");
   }
 });
 
 app.post('/profile/addDescription', async (req, res) => {
-  const newDescrition = req.body.dsc;
-  client.query('UPDATE users SET descriere = $1 WHERE usname = $2 RETURNING *', [newDescrition, username]);
-  const description = {descriere:newDescrition}; 
-  res.render('profile', {description});
+  try {
+    const newDescrition = req.body.dsc;
+    client.query('UPDATE users SET description = $1 WHERE usname = $2 RETURNING *', [newDescrition, username]);
+    const description = { description: newDescrition };
+    res.render('profile', { description });
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("Some server errors");
+  }
 });
 
-app.post('/profile/newCandidat', async (req, res) => {
-  client.query('INSERT INTO candidati (nume, voturi) VALUES ($1, $2)', [username, 0]);
+app.post('/profile/newCandidate', async (req, res) => {
+  try {
+    const document = await client.query('SELECT name FROM candidates WHERE name = $1', [username]);
+    if (document.rowCount === 0) {
+    client.query('INSERT INTO candidates (name, votes) VALUES ($1, $2)', [username, 0]);
+    res.send ("You are now on candidates list, congratulations")
+    } else {
+      res.send ("You are already on the list of candidates")
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(404).send("Some server errors");
+  }
+  
 });
 
-app.get('/list/seeuser', async (req, res) => {
+app.get('/list/seeCandidate', async (req, res) => {
   const { user } = req.query;
   try {
-    const response = await client.query(`SELECT descriere FROM users WHERE usname = $1`, [user])
-    const description = response.rows[0].descriere;
+    const response = await client.query(`SELECT description FROM users WHERE usname = $1`, [user])
+    const description = response.rows[0].description || "Description do not exist";
 
     res.status(200).send(description);
   } catch (error) {
@@ -86,28 +103,34 @@ app.get('/list/seeuser', async (req, res) => {
   }
 });
 
-app.get ('/list/vot', async (req, res) => {
-  const { user } = req.query;
-  const result = await client.query('SELECT voturi FROM candidati WHERE nume = ($1)', [user]);
-  const vot_number = result.rows[0].voturi;
-  const newVotNumber = vot_number + 1;
-  await client.query('UPDATE candidati SET voturi = $1 WHERE nume = $2', [newVotNumber, user]);
-  res.send('Votul a fost înregistrat cu succes!');
+app.get('/list/votes', async (req, res) => {
+  try {
+    const { user } = req.query;
+    const result = await client.query('SELECT votes FROM candidates WHERE name = ($1)', [user]);
+    const vot_number = result.rows[0].votes;
+    const newVotNumber = vot_number + 1;
+    await client.query('UPDATE candidates SET votes = $1 WHERE nume = $2', [newVotNumber, user]);
+    res.send('The vote was registered successfully!');
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("Some server errors");
+  }
 });
 
 app.get('/startpage/list', async (req, res) => {
   try {
-    const document = await client.query("SELECT nume FROM candidati ORDER BY voturi DESC ");
+    const document = await client.query("SELECT name FROM candidates ORDER BY votes DESC ");
     let variable = "";
     document.rows.forEach(row => {
-      variable += `${row.nume}
-        <button onclick="window.location.href='/list/seeuser?user=${row.nume}'">VIEW DESCRIPTION</button>
-        <button onclick="window.location.href='/vot?user=${row.nume}'">VOT</button>
+      variable += `${row.name}
+        <button onclick="window.location.href='/list/seeCandidate?user=${row.name}'">VIEW DESCRIPTION</button>
+        <button onclick="window.location.href='/vot?user=${row.name}'">VOTE</button>
         <br>`;
     });
     res.send(variable);
   } catch (error) {
     console.log(error);
+    res.status(404).send("Some server errors");
   }
 });
 
